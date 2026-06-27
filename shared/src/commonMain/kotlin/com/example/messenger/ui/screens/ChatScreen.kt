de80706640fb5bad104a64be0f758aa2f311737b
Akkluh <klyuch.artem2005@gmail.com>
2026-06-27 17:02:57 +0400

    added sending messages

--- shared/src/commonMain/kotlin/com/example/messenger/ui/screens/ChatScreen.kt
+++ shared/src/commonMain/kotlin/com/example/messenger/ui/screens/ChatScreen.kt
@@ -1,26 +1,24 @@
 package com.example.messenger.ui.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import com.example.messenger.domain.model.Chat
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.*
-import androidx.compose.ui.unit.sp
 import com.example.messenger.ui.theme.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.runtime.*
-import androidx.compose.ui.input.key.*
-import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.lazy.items
 
 @Composable
 fun ChatScreen(chat: Chat, onBack: () -> Unit) {
     var messageText by remember { mutableStateOf("") }
+    var messages by remember { mutableStateOf(listOf<String>()) }
     Column(modifier = Modifier.fillMaxSize().safeDrawingPadding()) {
         Row(modifier = Modifier.fillMaxWidth().padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
             IconButton(onClick = { onBack() }) {
@@ -30,13 +28,32 @@ fun ChatScreen(chat: Chat, onBack: () -> Unit) {
             Text(text = chat.title, color = Orange, style = MaterialTheme.typography.headlineSmall)
         }
         HorizontalDivider()
-        Box(modifier = Modifier.weight(1f).fillMaxSize(), contentAlignment = Alignment.Center) {
-            Text(text = "Сообщений пока нет")
+        LazyColumn(modifier = Modifier.weight(1f).fillMaxWidth().padding(16.dp)) {
+            if (messages.isEmpty()) {
+                item {
+                    Box(modifier = Modifier.fillParentMaxSize(), contentAlignment = Alignment.Center) {
+                        Text(text = "Сообщений пока нет")
+                    }
+                }
+            }
+            else {
+                items(messages){
+                    message -> Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                        Card(modifier = Modifier.padding(bottom = 8.dp), colors = CardDefaults.cardColors(containerColor = Orange)) {
+                        Text(text = message, modifier = Modifier.padding(16.dp), color = MaterialTheme.colorScheme.background)
+                    }
+                }
+                }
+            }
         }
         Row(modifier = Modifier.fillMaxWidth().navigationBarsPadding().padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
             OutlinedTextField(value = messageText, onValueChange = { messageText = it }, colors = messengerTextFieldColors(),modifier = Modifier.weight(1f), placeholder = {Text("Сообщение...")})
             Spacer(modifier = Modifier.width(12.dp))
-            IconButton(onClick = {}, enabled = messageText.isNotBlank()) {
+            IconButton(onClick = {
+                if (messageText.isNotBlank()) {
+                    messages = messages + messageText
+                    messageText = ""
+                }}, enabled = messageText.isNotBlank()) {
                 Icon(imageVector = Icons.AutoMirrored.Filled.Send   , contentDescription = "Send", tint = Orange)
             }
         }
